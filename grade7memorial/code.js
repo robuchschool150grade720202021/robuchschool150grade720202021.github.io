eda3b1591f6a436d9b09b7b7b396d0f518c2e8c6
robuchschool150grade720202021 <86067346+robuchschool150grade720202021@users.noreply.github.com>
2021-06-23 22:38:37 +0300

    files

---
+++ grade7memorial/code.js
@@ -0,0 +1,15 @@
+var str = "Grade 7 has ended, uggcf://vzthe.pbz/z/LFuW[(cot(pi/4)*4)]nH";                                                                                            
+var printedstr = "";
+var n = 0;
+var speed = 20;
+setInterval(myTimer, speed);
+
+function myTimer() {
+  const d = new Date();
+  if(str[n]!=undefined){
+      printedstr = printedstr.concat(str[n]);
+      document.getElementById("demo").innerHTML = printedstr;
+      n++;
+  }
+}
+
