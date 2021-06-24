63007bc07f5ff86371430078221084b69995ce23
sincaster <sindavmi@gmail.com>
2021-06-24 14:13:00 +0300

    happy birthday

--- grade7memorial/code.js
+++ grade7memorial/code.js
@@ -1,15 +1,19 @@
 var str = "Grade 7 has ended, uggcf://vzthe.pbz/n/LFuW[(cot(pi/4)*4)]nH";                                                                                            
 var printedstr = "";
+
 var n = 0;
-var speed = 20;
+const speed = 20;
 setInterval(myTimer, speed);
 
-function myTimer() {
-  const d = new Date();
-  if(str[n]!=undefined){
-      printedstr = printedstr.concat(str[n]);
-      document.getElementById("demo").innerHTML = printedstr;
-      n++;
-  }
+function myTimer()
+{
+    let i = 0;
+    const d = new Date();
+    if (str[n] != undefined)
+    {
+	printedstr = printedstr.concat(str[n]);
+	document.getElementById("demo").innerHTML = printedstr;
+	n++;
+    }
 }
 
